94eaff3ba3cea594e334fc5e2ed350c0236b5aa3
Cong Nguyen Thanh <thanhcong.bk10@gmail.com>
2021-01-13 01:02:19 +0700

    Updated

--- ckanext/dataconverter/plugin.py
+++ ckanext/dataconverter/plugin.py
@@ -8,6 +8,8 @@ import uuid
 import subprocess
 #import docker
 import configparser
+from werkzeug.datastructures import FileStorage
+import re
 
 class DataconverterPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
 
@@ -21,6 +23,7 @@ class DataconverterPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
     p.implements(p.IDatasetForm)
     p.implements(p.IConfigurer)
     p.implements(p.IResourceController, inherit=True)
+    p.implements(p.IValidators)
 
     def after_create(self, context, resource):
         self.config_path = f'/tmp/{self.uuid4}/config.ini'
@@ -57,6 +60,9 @@ class DataconverterPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
         os.system(f"mkdir /tmp/{self.uuid4}")
 
         if "network_config" in resource and "file_idl" in resource:
+            #network_config_validator(resource["network_config"])
+            #file_idl_validator(resource["file_idl"])
+
             network_config_path = f"/tmp/{self.uuid4}/rtps.ini"
             resource["network_config"].save(network_config_path)
             del resource["network_config"]
@@ -69,6 +75,8 @@ class DataconverterPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
             self.env["network_config"] = network_config_path
             self.env["file_idl"] = file_idl_path
             self.common_config['converter'] = 'dds'
+
+
         if not resource["name"] and "topic_name" in resource:
             resource["name"] = resource["topic_name"]
         print("*************************************")
@@ -112,16 +120,22 @@ class DataconverterPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
         # Add our custom_test metadata field to the schema, this one will use
         # convert_to_extras instead of convert_to_tags.
         # schema.update({
-        #         'cong_field1': [tk.get_validator('ignore_missing'),
+        #         'cong_field1': [tk.get_validator('not_missing'),
         #             tk.get_converter('convert_to_extras')]
         #         })
         # # Add our custom_resource_text metadata field to the schema
-        # schema['resources'].update({
-        #         'file_idl' : [ tk.get_validator('ignore_missing') ]
-        #         })
-        # schema['resources'].update({
-        #         'network_config' : [ tk.get_validator('ignore_missing') ]
-        #     })
+        schema['resources'].update({
+                'file_idl' : [ tk.get_validator('ignore_missing'),
+                                tk.get_converter('convert_to_extras')]
+                })
+        schema['resources'].update({
+                'network_config' : [ tk.get_validator('ignore_missing'),
+                                        tk.get_converter('convert_to_extras')]
+            })
+        schema['resources'].update({
+                'topic_name' : [ tk.get_validator('topic_name_validator'),
+                                    tk.get_converter('convert_to_extras')]
+            })
         return schema
 
     def create_package_schema(self):
@@ -137,21 +151,22 @@ class DataconverterPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
 
     def show_package_schema(self):
         schema = super(DataconverterPlugin, self).show_package_schema()
-        # schema.update({
-        #     'cong_field1': [tk.get_converter('convert_from_extras'),
-        #                     tk.get_validator('ignore_missing')]
-        # })
-
-        # schema['resources'].update({
-        #         'file_idl' : [ tk.get_validator('ignore_missing') ]
-        #     })
-
-        # schema['resources'].update({
-        #         'network_config' : [ tk.get_validator('ignore_missing') ]
-        #     })
-        # print("^^^^^^^^^^^^^^^^^^^^^^^^")
-        # print(schema)
-        # print("^^^^^^^^^^^^^^^^^^^^^^^^")
+        
+        schema['resources'].update({
+                'file_idl' : [ tk.get_validator('ignore_missing'),
+                                tk.get_converter('convert_to_extras')]
+                })
+        schema['resources'].update({
+                'network_config' : [ tk.get_validator('ignore_missing'),
+                                        tk.get_converter('convert_to_extras')]
+            })
+        schema['resources'].update({
+                'topic_name' : [ tk.get_validator('topic_name_validator'),
+                                    tk.get_converter('convert_to_extras')]
+            })
+        print("^^^^^^^^^^^^^^^^^^^^^^^^")
+        print(schema)
+        print("^^^^^^^^^^^^^^^^^^^^^^^^")
         return schema
 
     def is_fallback(self):
@@ -163,3 +178,74 @@ class DataconverterPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
         # This plugin doesn't handle any special package types, it just
         # registers itself as the default (above).
         return []
+
+    # def validate(self, context, data_dict, schema, action):
+    #     file_idl = data_dict.get("file_idl")
+    #     network_config = data_dict.get("network_config")
+    #     topic_name = data_dict.get("topic_name")
+    #     err_dict = dict()
+    #     if file_idl is not None and type(file_idl) is not FileStorage:
+    #         err_dict["file_idl"] = ["IDL file is missing"]
+    #     if network_config is not None and type(network_config) is not FileStorage:
+    #         err_dict["network_config"] = ["Network config file is missing"]
+    #     if topic_name == "":
+    #         err_dict["topic_name"] = ["Topic name is missing"]
+
+    #     result = super(DataconverterPlugin, self).validate(context, data_dict, schema, action)
+    #     print("type", type(result))
+    #     print("validate", result)
+    #     #return (data_dict, err_dict)
+    #     return result
+
+    def get_validators(self):
+        return {
+            u'file_idl_validator': file_idl_validator,
+            u'network_config_validator': network_config_validator,
+            u'topic_name_validator': topic_name_validator,
+        }
+
+
+def file_idl_validator(key, data, errors, context):
+    value = data.get(key)
+    print("#################file_idl_validator#################")
+    print(data)
+    print(type(value))
+    print(errors)
+    print("@@@@@@@@@@@@@@@@@file_idl_validator@@@@@@@@@@@@@@@@@")
+    if value is not None and type(value) is not FileStorage:
+        raise tk.Invalid(u"IDL file isn't uploaded")
+    elif type(value) is FileStorage:
+        tmp_uuid = uuid.uuid4().hex
+        os.system(f"mkdir /tmp/{tmp_uuid}")
+        tmp_idl_file = f"/tmp/{tmp_uuid}/Messenger.idl"
+        value.save(tmp_idl_file)
+        command = f"docker exec source_opendds-ckan_1 python3 ./source/run.py check -i {tmp_idl_file}"
+        result = subprocess.check_output(command, shell=True).decode()
+        if result.strip() != "valid":
+            raise tk.Invalid(u"IDL file is invalid. Please check!")
+    return value
+
+def network_config_validator(key, data, errors, context):
+    value = data.get(key)
+    if value is not None and type(value) is not FileStorage:
+        raise tk.Invalid(u"Network config file isn't uploaded")
+    elif type(value) is FileStorage:
+        tmp_uuid = uuid.uuid4().hex
+        os.system(f"mkdir /tmp/{tmp_uuid}")
+        tmp_ini_file = f"/tmp/{tmp_uuid}/rtps.ini"
+        value.save(tmp_ini_file)
+        try:
+            config = configparser.ConfigParser()
+            config.read_file(open(tmp_ini_file))
+        except configparser.Error:
+            raise tk.Invalid(u"Network config file is invalid. Please check!")
+    return value
+
+def topic_name_validator(value):
+    if value == "":
+        raise tk.Invalid(u"Topic name is missing")
+    elif type(value) is str:
+        x = re.match(r'[_a-zA-z][_a-zA-z0-9]*', value)
+        if x is None:
+            raise tk.Invalid(u"Topic name is invalid. Please check!")
+    return value
